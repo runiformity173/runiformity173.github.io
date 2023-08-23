a76fade7639002f60dc8912bc148d0640f877368
Eli Harrison <88563724+ezhgamer173@users.noreply.github.com>
2023-08-23 08:36:39 -0400

    Tables help.

---
+++ dnd/Tables/help.js
@@ -0,0 +1,8 @@
+const helpText = `Set "Filter" to something to only display categories in that filter.
+Select a category to generate in "Category".
+Select a level of the category in "Challenge Rating".
+'0-4','5-10','11-16', and '17-20' for non-dragon hoards.
+'Wyrmling', 'Young', 'Adult', and 'Ancient' for dragon hoards.
+Press "Roll!" to get an output of the encounter or treasure.
+
+If it doesn't work, reload. Make sure that your "Challenge Rating" value isn't a dragon value for a non-dragon category, and vice versa.`;
