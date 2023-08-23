68c6f55d40a828303e6002a46bff17d419f09892
Eli Harrison <88563724+ezhgamer173@users.noreply.github.com>
2023-08-23 08:19:16 -0400

    Attempted help button fix

--- sharedScripts/help.js
+++ sharedScripts/help.js
@@ -4,7 +4,7 @@ overlay.style.width = "100%";
 overlay.style.height = "100%";
 overlay.style.opacity = 0.0;
 overlay.style.transition = "opacity 0.2s ease-out";
-overlay.style.backgroundColor = "#292521";
+overlay.style.backgroundColor = "#212529";
 overlay.style.cursor = "pointer";
 overlay.style.color = "#ffffff";
 overlay.style.textAlign = "center";
@@ -12,16 +12,16 @@ overlay.style.display = "none";
 overlay.style.position = "absolute";
 overlay.style.top = "0";
 overlay.style.left = "0";
-overlay.innerHTML = "<p>"+helpText.replaceAll("\n","<br>")+"</p>"
-overlay.firstElementChild.style.opacity = "2.0";
+overlay.innerHTML = "<p style='font-family:verdana;'>"+helpText.replaceAll("\n","<br>")+"</p>"
+// overlay.firstElementChild.style.opacity = "1.0";
 let helpIsDisplayed = false;
 document.onkeyup = function(e) {
   if (e.keyCode == 72 && e.altKey) {
   
     if (!helpIsDisplayed) {
+      overlay.style.opacity = 0.0;
       overlay.style.display = "block";
-      overlay.style.transition = "opacity 0.2s ease-out";
-      setTimeout(function(){overlay.style.opacity = 0.75;},0);
+      setTimeout(function(){overlay.style.opacity = 1.0;},0);
     } else {
       overlay.style.opacity = 0.0;
       setTimeout(function(){overlay.style.display = "none";},200);
