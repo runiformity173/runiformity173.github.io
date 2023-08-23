12d24fe10489c8ff2ae25f3b7827be51443f8e09
Eli Harrison <88563724+ezhgamer173@users.noreply.github.com>
2023-08-23 10:05:53 -0400

    Last help for DND websites

---
+++ dnd/WeaponGenerator/help.js
@@ -0,0 +1,6 @@
+const helpText = `If "Override Chances" is set to -1, then the chances for each upgrade type is the chance that that will be present.
+If "Override Chances" is set to another number, that is how many modifiers will be added. (2 works great)
+"Weapon" is the weapon, set to 'random' for a random weapon.
+
+Press "Roll" to get the weapon.
+It will give the name and properties of the item.`;
