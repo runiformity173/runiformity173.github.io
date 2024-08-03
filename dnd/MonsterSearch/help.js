d99c1a6547016f27bc0d2d91bb16b408b6ca4295
Eli Harrison <88563724+runiformity173@users.noreply.github.com>
2024-08-03 15:10:54 -0400

    Added help text for Screen and Monster Search and redid menu

---
+++ dnd/MonsterSearch/help.js
@@ -0,0 +1,10 @@
+const helpText = `To search for a monster, set the filters then press "Filter".
+Checking select all will set all in that category to the value of the select all checkbox.
+You can filter by the challenge rating range, alignment, speeds it has, senses it has, and monster type.
+Click on any monster in the output to view it.
+
+A lot of checkboxes have 3 values. The default is the "-", meaning unset. This will include monsters that have and do not have this trait.
+The checked state means that the result will only include monsters that do have that trait.
+The unchecked state means that the result will only include monsters that do NOT have that trait.
+
+The slider is challenge rating range by the way`;
