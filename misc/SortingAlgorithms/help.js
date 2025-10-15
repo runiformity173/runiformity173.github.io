9046969967c74dd0590e7033bf734da405733a10
runiformity173 <eli.z.harrison173@gmail.com>
2025-10-15 13:51:07 -0400

    SortingAlgorithms: Portfolio instructions

---
+++ misc/SortingAlgorithms/help.js
@@ -0,0 +1,11 @@
+const helpText = "asdf";
+window.portfolioText = `<h1>Instructions</h1>
+Click the "Sort" button on the top right to sort! (There's sound too)<br>
+Use the selector to change which sorting algorithm is used.<br>
+Use the slider to adjust how many items you're sorting. (But be warned... you will get an alert if you try to sort too large a dataset with an inefficient sorting algorithm.)<br>
+You can change the display to be colored bars (default), just the colors, or just the bars.<br>
+If you want to, you can further customize the display by flipping it to vertical or adjusting the gradient used.<br>
+<h3>About</h3>
+The algorithms are all implemented by me, although none of them are my invention (I came up with anytime quicksort independently but I'm sure it already exists).<br>
+Mergesort is displayed as if it were an in-place sort to highlight the process of merging, but in practical uses it would be implemented with auxillary memory instead of as visualized.<br>
+`;
