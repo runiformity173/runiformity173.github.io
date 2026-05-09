a60d0c6cc49fefe3707b4dcdce048cc1c3b985a9
runiformity173 <eli.z.harrison173@gmail.com>
2026-05-09 12:09:03 -0400

    Move DMScreen2 to its own repo

--- dnd/DMScreen2/monsters.js
+++
@@ -1,5 +0,0 @@
-let monsterDatalist = "";
-for (const monster of monsters) {
-    monsterDatalist += `<option>${monster}</option>`;
-}
-document.getElementById("monstersDatalist").innerHTML = monsterDatalist;
--- dnd/DMScreen2/spells.js
+++
@@ -1,5 +0,0 @@
-let spellDatalist = "";
-for (const spell of spells) {
-  spellDatalist += `<option>${spell}</option>`;
-}
-document.getElementById("spellsDatalist").innerHTML = spellDatalist;
